e3740a000829a407c26f1b5dcb593e0bac3ca36b
mkoregin <mkoregin@mail.ru>
2020-01-12 15:50:43 +0300

    second

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public Double GetGrade(int Index)
+        {
+            return grades[Index];
+        }
+        public Double GetAverage()
+        {
+            double Sum = 0;
+            foreach (double i in grades)
+            {
+                Sum += i;
+            }
+            return Sum/grades.Count;
+        }
+
+
+        List<Double> grades;
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Book = new Book();
+            Book.AddGrade(10.1);
+            Book.AddGrade(20.1);
+
+            Console.WriteLine(Book.GetAverage());
+        }
+    }
+}
